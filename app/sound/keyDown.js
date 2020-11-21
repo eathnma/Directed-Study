61294b37ed4f78426f0082d9bc4e73a321f5eb7d
Ethan <ethanmahc@gmail.com>
2020-11-21 12:54:53 -0800

    Tone.Js Rough Implementation
    Keyboard Sound works. Need to make the sounds artistic & harmonious

--- app/sound/keyDown.js
+++ app/sound/keyDown.js
@@ -9,13 +9,26 @@ function setMyKeyDownListener() {
     )
 }
 
+// play synth sound
+// Membrane Synth https://tonejs.github.io/docs/r12/MembraneSynth
+const synth = new Tone.MembraneSynth().toMaster();
+function playSynth() {
+    synth.triggerAttackRelease("C3", "7n");
+}
+
 //tracks keys
 function logKey (key) {
     console.log("Key pressed is: "+ key);
-
+    
     // grab content from html
     var keyPrintElement = document.getElementById("keyPrint");
     // append string to the key tab
     keyPrintElement.insertAdjacentHTML('beforeend',key);
+    playSynth();
     
 }
+
+
+
+
+ 
