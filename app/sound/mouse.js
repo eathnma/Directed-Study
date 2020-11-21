7430515be33772c79f8f6d3568de08172c879730
Ethan <ethanmahc@gmail.com>
2020-11-21 15:15:15 -0800

    Removed Typescript & Added Paper.Js
    Paper JS for vector graphics. Specifically tracking mouse movements

--- app/sound/mouse.js
+++ app/sound/mouse.js
@@ -1,6 +1,6 @@
 // All mouse related events go into here.
 
-// 1. Create an event listener & run function if clicked
+// 1. Create a window event listener & run function if clicked
 document.addEventListener("click",function(){
     console.log("mouse clicked");
     playSynth();
