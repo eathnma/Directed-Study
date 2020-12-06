3488da90f00f912a2c99ad3b6d33c24110d900e1
Ethan <ethanmahc@gmail.com>
2020-12-06 00:02:02 -0800

    Added Chords to JS
    Now time to implement random switching

--- app/sound/keyboard.js
+++ app/sound/keyboard.js
@@ -1,36 +1,62 @@
-// puts listener whenever you press the keyboard
-// use addEventListener, because event.keyCode is deprecated
-function setMyKeyDownListener() {
-    window.addEventListener(
-      "keydown",
-      function(event) {
-          logKey(event.key);
-        }
-    )
-}
+// https://devriffs.com/simple-tonejs-keyboard/
 
-// play synth sound
-// Membrane Synth https://tonejs.github.io/docs/r12/MembraneSynth
-// const synth = new Tone.MembraneSynth().toMaster();
-// const player = new Tone.Player("https://tonejs.github.io/audio/berklee/gong_1.mp3").toDestination();
+var CMin7 = ["C5", "G5", "Eb5", "Bb5"];
+var BMaj7 = ["B5", "Gb5", "Eb5", "Bb6"];
+var BbMin7 = ["Bb5", "F5", "Db5", "Ab5"];
+var AMaj7 = ["A5", "E5", "Db5", "Ab6"];
+var CsMin9 = ["Db5", "B5", "Eb5", "Ab6"];
 
 const sampler = new Tone.Sampler({
 	urls: {
-        A1: "A1.mp3",
-		A2: "A2.mp3",
+        A3: "audio/piano/a3.mp3",
+        A4: "audio/piano/a4.mp3",
+        A5: "audio/piano/a5.mp3",
 	},
-	baseUrl: "https://tonejs.github.io/audio/casio/",
 }).toDestination();
 
-const synth = new Tone.Synth().toDestination();
-
-function playSynth() {
-    sampler.triggerAttackRelease(["C1", "G1", "Eb1", "Bb1"], 4);
+function chordRandomizer(){
+    // randomize the array
+    var chordArray = [CMin7, BMaj7, BbMin7, AMaj7, CsMin9];
+    const randomChord = chordArray[Math.floor(Math.random() * chordArray.length)];
+    return randomChord;
 }
 
-window.addEventListener('click');
+// puts listener whenever you press the keyboard
+function onKeyDown(){
+window.addEventListener("keydown",
+    function(event) {
+        logKey(event.key);
+        console.log(chordRandomizer());
+        lookForKeys(event, chordRandomizer());
+    }
+    );
+}
 
-//tracks keys
+
+function lookForKeys(event, chord){
+    if(event.key == 'a'){
+        sampler.triggerAttackRelease(chord[0], 1);
+    }
+
+    if(event.key == 's'){
+        sampler.triggerAttackRelease(chord[1], 1);
+    }
+
+    if(event.key == 'd'){
+        sampler.triggerAttackRelease(chord[2], 1);
+    }
+    
+    if(event.key == 'f'){
+        sampler.triggerAttackRelease(chord[3], 1);
+    }
+    
+    // if(event.key == 'g'){
+    //     sampler.triggerAttackRelease(chord[4], 1);
+    // }
+}
+
+// window.addEventListener('click');
+
 function logKey (key) {
     console.log("Key pressed is: "+ key);
     
@@ -38,19 +64,5 @@ function logKey (key) {
     var keyPrintElement = document.getElementById("keyPrint");
     // append string to the key tab
     keyPrintElement.insertAdjacentHTML('beforeend',key);
-    playSynth();
-    
-}
-
-// for when we need to load items 
-try{
 
 }
-catch(err){
-
-}
-
-
-
-
- 
--- app/sound/mouse.js
+++ app/sound/mouse.js
@@ -1,46 +1,46 @@
-// All mouse related events go into here.
+// // All mouse related events go into here.
 
-// 1. Create a window event listener & run function if clicked
-document.addEventListener("click",function(){
-    console.log("mouse clicked");
-    playSynth();
-});
+// // 1. Create a window event listener & run function if clicked
+// document.addEventListener("click",function(){
+//     console.log("mouse clicked");
+//     // playSynth();
+// });
 
-// PAPER.JS EXAMPLE CHAIN 
-// The amount of points in the path:
-var points = 25;
-// The distance between the points:
-var length = 35;
+// // PAPER.JS EXAMPLE CHAIN 
+// // The amount of points in the path:
+// var points = 25;
+// // The distance between the points:
+// var length = 35;
 
-// create new path
-var path = new Path({
-	strokeColor: '#E4141B',
-	strokeWidth: 10,
-	strokeCap: 'round'
-});
+// // create new path
+// var path = new Path({
+// 	strokeColor: '#E4141B',
+// 	strokeWidth: 10,
+// 	strokeCap: 'round'
+// });
 
-var start = view.center / [10, 1];
-for (var i = 0; i < points; i++)
-	path.add(start + new Point(i * length, 0));
+// var start = view.center / [10, 1];
+// for (var i = 0; i < points; i++)
+// 	path.add(start + new Point(i * length, 0));
 
-function onMouseMove(event) {
-	path.firstSegment.point = event.point;
-	for (var i = 0; i < points - 1; i++) {
-		var segment = path.segments[i];
-		var nextSegment = segment.next;
-		var vector = segment.point - nextSegment.point;
-		vector.length = length;
-		nextSegment.point = segment.point - vector;
-	}
-	path.smooth({ type: 'continuous' });
-}
+// function onMouseMove(event) {
+// 	path.firstSegment.point = event.point;
+// 	for (var i = 0; i < points - 1; i++) {
+// 		var segment = path.segments[i];
+// 		var nextSegment = segment.next;
+// 		var vector = segment.point - nextSegment.point;
+// 		vector.length = length;
+// 		nextSegment.point = segment.point - vector;
+// 	}
+// 	path.smooth({ type: 'continuous' });
+// }
 
-function onMouseDown(event) {
-	path.fullySelected = true;
-	path.strokeColor = '#e08285';
-}
+// function onMouseDown(event) {
+// 	path.fullySelected = true;
+// 	path.strokeColor = '#e08285';
+// }
 
-function onMouseUp(event) {
-	path.fullySelected = false;
-	path.strokeColor = '#e4141b';
-}
+// function onMouseUp(event) {
+// 	path.fullySelected = false;
+// 	path.strokeColor = '#e4141b';
+// }
