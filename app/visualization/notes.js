938a494f9997ee5615165c04fc95969adafa8bde
Ethan <ethanmahc@gmail.com>
2020-12-12 21:00:11 -0800

    Basic UI + Clicking Events Completed

--- app/visualization/notes.js
+++ app/visualization/notes.js
@@ -1,22 +1,24 @@
-// get reference from draw group 
-var elem = document.getElementById('draw-group');
-var two = new Two({ width: 285, height: 200 }).appendTo(elem);
+// Make an instance of two and place it on the page.
+var elem = document.getElementById('draw-shapes');
+var params = { width: 10, height: 370 };
+var two = new Two(params).appendTo(elem);
 
-var circle = two.makeCircle(-70, 0, 50);
-var rect = two.makeRectangle(70, 0, 100, 100);
-circle.fill = '#FF8000';
-circle.stroke = 'orangered';
-rect.fill = 'rgba(0, 200, 255, 0.75)';
-rect.stroke = '#1C75BC';
+// two has convenience methods to create shapes.
+var rectWidth = 5;
+var rectHeight = 20;
+var rect = two.makeRoundedRectangle(20 + rectWidth/2 , 0 + rectHeight /2, rectWidth, rectHeight);
 
-// Groups can take an array of shapes and/or groups.
-var group = two.makeGroup(circle, rect);
+// The object returned has many stylable properties:
+
+rect.fill = 'rgb(0, 200, 255)';
+rect.opacity = 0.75;
+rect.noStroke();
 
 var yPos = 0;
 // make shapes move upwards
 two.bind('update', function(frameCount) {
 yPos--;
-group.translation.set(two.width / 2, two.height / 2 + yPos);
+rect.translation.set(two.width / 2, 370 + yPos);
 }).play();  // Finally, start the animation loop
 
 two.update();
